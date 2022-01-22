a82562823eb9a9ecfd908623f1ac98d589509cfc
hendridg <hendridg@gmail.com>
2022-01-22 14:52:08 -0400

    Revert API KEY

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './app'
-import { GlobalStyle } from './global-styles'
-import { FirebaseContext } from './context/firebase'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './app';
+import { GlobalStyle } from './global-styles';
+import { FirebaseContext } from './context/firebase';
 // import { seedDatabase } from './seed'
 
 const config = {
-    apiKey: API_KEY,
-    authDomain: "netflix-clone-81c3f.firebaseapp.com",
-    databaseURL: "https://netflix-clone-81c3f-default-rtdb.firebaseio.com",
-    projectId: "netflix-clone-81c3f",
-    storageBucket: "netflix-clone-81c3f.appspot.com",
-    messagingSenderId: "486335656711",
-    appId: API_ID,
-}
+  apiKey: 'AIzaSyAIrJn4GBwNGhcf3TRTAA_3KaEBmZts8jo',
+  authDomain: 'netflix-clone-81c3f.firebaseapp.com',
+  databaseURL: 'https://netflix-clone-81c3f-default-rtdb.firebaseio.com',
+  projectId: 'netflix-clone-81c3f',
+  storageBucket: 'netflix-clone-81c3f.appspot.com',
+  messagingSenderId: '486335656711',
+  appId: '1:486335656711:web:7cff99005956e460454ba2',
+};
 
-const firebase = window.firebase.initializeApp(config)
+const firebase = window.firebase.initializeApp(config);
 
 // seedDatabase(firebase)
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
-        <GlobalStyle />
-        <App />
-    </FirebaseContext.Provider>, 
-    document.getElementById('root')
-)
+  <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+    <GlobalStyle />
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
